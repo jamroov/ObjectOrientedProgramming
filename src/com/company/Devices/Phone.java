08eb00b5f1783db39ed6083874f868a3f2f79557
jamroov <jamroov@gmail.com>
2020-03-01 11:03:39 +0100

    Add comments

--- src/com/company/Devices/Phone.java
+++ src/com/company/Devices/Phone.java
@@ -8,6 +8,9 @@ public class Phone extends Device {
         this.screenSize = screenSize;
     }
 
+    //Abstract class: you know what you need - you can only extend one class
+    //Interface: you don't know what you need but you know that you will need a lot of it. like all things here can be sold,
+    //how to sell depends on more granualr implementation in each class. - you can imlpement any number of interfaces
 
     @Override
     public void turnOn() {
