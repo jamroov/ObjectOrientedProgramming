6a088b9786b8e10752ffd9da3049ef8dabf1ceb4
Mateusz <jamroov@gmail.com>
2021-02-26 19:54:55 +0100

    Also update Pet class

--- src/com/company/Animals/Pet.java
+++ src/com/company/Animals/Pet.java
@@ -3,8 +3,8 @@ package com.company.Animals;
 import java.sql.SQLException;
 
 public class Pet extends Animal {
-    public Pet(String specie, Double weight, String name, Double price, Gender gender) throws SQLException {
-        super(specie, weight, name, gender);
+    public Pet(String specie, Double weight, String name, Double price, Gender gender, foodType foodType) throws SQLException {
+        super(specie, weight, name, gender, foodType);
         this.Price = price;
         //super.save();
     }
